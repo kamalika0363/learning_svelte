7d3409906971d69283fe2a0e7be46dfe568fa366
kamalika0363 <59131714+kamalika0363@users.noreply.github.com>
2023-11-12 12:42:47 +0530

    learning svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,62 @@
+<script>
+    import '../styles.css';
+    import logo from '$lib/images/logo.svg';
+    import tmdb from '$lib/images/tmdb.svg';
+</script>
+<nav>
+    <a href = "/">
+        <img src={logo} alt="SvelteFlix Logo">
+    </a>
+
+    <div class="links">
+        <a href = "/search">About</a>
+        <a href = "/watchlist">watch List</a>
+        <a href = "/login">Login</a>
+    </div>
+</nav>
+
+<main>
+    <slot />
+</main>
+
+
+<footer>
+    <a href="https:www.moviedb.org/">
+        Data Provided by
+        <img src={tmdb} alt="footer"> 
+    </a>
+</footer>
+<style>
+    nav {
+        display: flex;
+        width: 100%;
+        height: 3rem;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        align-items: center;
+        justify-content: space-between;
+        max-width: var(--column);
+        padding: 0 var(--side);
+        color :var(--accent);
+    }
+
+    a{
+        color: inherit;
+        text-decoration: none;
+    }
+
+    img{
+        height: 1.5rem;
+        margin-top: 30px;
+    }
+
+    .links {
+        display: flex;
+        gap: 1rem;
+    }
+
+    footer {
+        display: flex;
+        justify-content: center;
+        height: 2rem;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Carousel from "$lib/components/Carousel.svelte";
+	import Hero from "./Hero.svelte";
+    import { views } from "$lib/views";
+
+    export let data;
+    console.log(data);
+</script>
+
+<h1 class="column">
+    Top Trending Movies
+</h1>
+
+<Hero movie={data.featured} />
+
+<Carousel movies={data.trending} view={views.trending} href="/movie/trending" />
+<Carousel movies={data.now_playing} view={views.now_playing}  href="/movie/now_playing" />
+<Carousel movies={data.upcoming} view={views.upcoming}  href="/movie/upcoming" />
